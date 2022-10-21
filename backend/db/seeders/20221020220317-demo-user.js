3a01453a618e44a747aa787332ade8de301917b6
hoon <isaacki1003@gmail.com>
2022-10-21 12:42:35 -0700

    updated seeder file

--- backend/db/seeders/20221020220317-demo-user.js
+++ backend/db/seeders/20221020220317-demo-user.js
@@ -7,16 +7,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Isaac',
+        lastName: 'Ki',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: 'Hoon',
+        lastName: 'Kim',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: 'Leyland',
+        lastName: 'Chin',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
