581452547a2dde7624ea4c6fb734798abd4f5621
hoon <isaacki1003@gmail.com>
2023-01-30 11:18:39 -0800

    added seeders for bookings

--- backend/db/migrations/20221024225530-create-booking.js
+++ backend/db/migrations/20221024225530-create-booking.js
@@ -36,11 +36,11 @@ module.exports = {
         onDelete: 'CASCADE'
       },
       startDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         allowNull: false
       },
       endDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         allowNull: false
       },
       createdAt: {
