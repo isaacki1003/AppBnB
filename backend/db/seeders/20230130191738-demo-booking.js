e39e4641f94d1b4390696810f5864e641570ef0f
hoon <isaacki1003@gmail.com>
2023-02-06 00:21:27 -0800

    render debugging

--- backend/db/seeders/20230130191738-demo-booking.js
+++ backend/db/seeders/20230130191738-demo-booking.js
@@ -1,8 +1,14 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		await queryInterface.bulkInsert('Bookings', [
+	up: (queryInterface, Sequelize) => {
+		options.tableName = 'Bookings';
+		return queryInterface.bulkInsert(options, [
 			{
 				spotId: 2,
 				userId: 1,
@@ -30,7 +36,8 @@ module.exports = {
 		]);
 	},
 
-	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete('Bookings', null, {});
+	down: (queryInterface, Sequelize) => {
+		options.tableName = 'Bookings';
+		return queryInterface.bulkDelete(options);
 	}
 };
